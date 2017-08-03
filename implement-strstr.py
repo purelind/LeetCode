fd51b40d53b8e5f964c196340ee759887284ec32
purelind <kevinsocial@outlook.com>
2017-08-03 19:01:36 +0800

    description for some code

--- implement-strstr.py
+++ implement-strstr.py
@@ -6,14 +6,14 @@ class Solution(object):
         :rtype: int
         """
 
-        if not haystack and not needle:
+        if not haystack and not needle:  # 边界检查,空字符串
             return 0
 
-        for i in range(len(haystack)-len(needle)+1):
+        for i in range(len(haystack)-len(needle)+1):  # 源字符串剩余长度必须大于目标字符串
             for j in range(len(needle)):
                 if haystack[i+j] != needle[j]:
                     break
-            else:
+            else:  # not break
                 return i
         return -1
 
