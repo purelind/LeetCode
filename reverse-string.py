6a34109ce303d0f1efd7e97b08d423380903e099
purelind <kevinsocial@outlook.com>
2017-08-11 16:12:03 +0800

    reverse string

---
+++ reverse-string.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return reversed()
+
+solve = Solution()
+print(solve.reverseString("hello"))
