72d0f2f4370928992d380182b6c9eb16c5792f0c
purelind <kevinsocial@outlook.com>
2017-11-05 17:23:00 +0800

    152. Maximum Product Subarray

--- maximum_product_subarray.py
+++ maximum_product_subarray.py
@@ -1,42 +1,20 @@
-"""
-152. Maximum Product Subarray
-Find the contiguous subarray within an array (containing at least one
-number) which has the largest product.
-For example, given the array [2, 3, -2, 4],
-the contiguous subarray [2, 3] has the largest product = 6
-"""
-# def maxProduct(nums):
-#     """
-#
-#     :param nums: List[int]
-#     :return: int
-#     """
-#     i = 0
-#     if len(nums) == 1:
-#         return nums[0]
-#     else:
-#         maxValue = nums[i] * nums[i+1]
-#         i += 1
-#         while i < len(nums)-1:
-#             test_Value = nums[i] * nums[i+1]
-#             if test_Value > maxValue:
-#                 maxValue = test_Value
-#             i += 1
-#         return maxValue
-#
-# print(maxProduct([2, 3, 2, 2]))
-
-
-def maxProduct(nums):
-    """
-
-    :param nums: List[int]
-    :return: int
-    """
-    def find_contiguous_positive_subarray(array):
+class Solution(object):
+    def maxProduct(self, nums):
         """
-        
-        :param array: List[int] 
-        :return: List[list]
+        :type nums: List[int]
+        :rtype: int
         """
+        r = nums[0]
+
+        imax = r
+        imin = r
+
+        for i in range(1, len(nums)):
+            if nums[i] < 0:
+                imax, imin = imin, imax
+            imax = max(nums[i], imax*nums[i])
+            imin = min(nums[i], imin*nums[i])
+            r = max(r, imax)
+        return r
+
 
